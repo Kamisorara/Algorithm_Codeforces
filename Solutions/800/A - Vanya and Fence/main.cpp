85f8019f513621517d65b0f5d5b893129128b84e
kamisora <1210281722@qq.com>
2022-11-18 15:51:01 +0800

    1.0

--- Solutions/800/A - Vanya and Fence/main.cpp
+++ Solutions/800/A - Vanya and Fence/main.cpp
@@ -1,6 +1,7 @@
+
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define endl '\n'
 #define INF 0x3f3f3f3f
 #define MOD le9 + 7
@@ -8,20 +9,26 @@ using namespace std;
 #define sdd(n, m) scanf("%d %d", &n, &m)
 #define mem(a, b) memset((a), (b), sizeof(a))
 #define rep(i, a, b) for(int i = (a); i <= (b); i++)
-#define fast_io                       \
-    ios_base::sync_with_stdio(false); \
-    cin.tie(0)
+#define io ios::sync_with_stdio(false)
+#define io_tie cin.tie(0)
 #define debug(a) cout << "Debuging...|" << #a << ": " << a << "\n";
 #define MAX 500000 + 50;
 typedef long long LL;
-
-int T;
-
-int main() {    
-    fast_io;
-    cin >> T;
-    while (T--) cout << T << endl;
+ 
+ 
+int n ,h;
+int q[2010];
+ 
+int main() {
+    io;
+    io_tie;
+    cin >> n >> h; 
+    for (int i = 0; i < n; i++) cin >> q[i];
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (q[i] <= h) cnt++;
+        else cnt += 2;
+    }
+    cout << cnt << endl;
     return 0;
 }
-
-
