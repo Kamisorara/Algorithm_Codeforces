4920a9c7b596290a92911fa12a1a7e0420da377b
kamisora <1210281722@qq.com>
2022-12-09 12:42:39 +0800

    1.0

--- Solutions/1300/1746C - Permutation Operations/main.cpp
+++ Solutions/1300/1746C - Permutation Operations/main.cpp
@@ -1,10 +1,24 @@
 #include <bits/stdc++.h>
- 
 using namespace std;
+
+#define endl '\n'
+#define INF 0x3f3f3f3f
+#define MOD le9 + 7
+#define sd(n) scanf("%d", &n)
+#define sdd(n, m) scanf("%d %d", &n, &m)
+#define mem(a, b) memset((a), (b), sizeof(a))
+#define rep(i, a, b) for (int i = (a); i <= (b); i++)
+#define fast_io                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define debug(a) cout << "Debuging...|" << #a << ": " << a << "\n";
+#define MAX 500000 + 50;
+typedef long long ll;
  
 int T, n;
  
 int main() {
+    fast_io;
     cin >> T;
     while (T--) {
         cin >> n;
